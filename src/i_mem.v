f59ddb0dd4bc831cba439540da19964e561f0c3b
MarioJam <mj.andrade.moreira@gmail.com>
2026-06-15 01:45:33 -0300

    feat: implement instruction memory

--- src/i_mem.v
+++ src/i_mem.v
@@ -1,2 +1,12 @@
-module i_mem;
+module i_mem #(parameter MEM_FILE = "programs/instruction.list")(
+    input  wire [31:0] address,
+    output wire [31:0] i_out
+);
+    reg [31:0] mem [0:1023];
+
+    initial begin
+        $readmemb(MEM_FILE, mem);
+    end
+
+    assign i_out = mem[address >> 2];
 endmodule
