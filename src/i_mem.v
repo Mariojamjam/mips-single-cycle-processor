b0477fe8fffb59ec42bac1737847779adeeec122
MarioJam <mj.andrade.moreira@gmail.com>
2026-06-15 02:00:21 -0300

    fix: added parametrization to i_mem

--- src/i_mem.v
+++ src/i_mem.v
@@ -1,12 +1,17 @@
-module i_mem #(parameter MEM_FILE = "programs/instruction.list")(
+module i_mem #(
+    parameter DEPTH    = 1024,
+    parameter MEM_FILE = "programs/instruction.list"
+)(
     input  wire [31:0] address,
     output wire [31:0] i_out
 );
-    reg [31:0] mem [0:1023];
+    reg [31:0] mem [0:DEPTH-1];
 
     initial begin
         $readmemb(MEM_FILE, mem);
     end
 
+    // Asynchronous read, divides byte address by 4 to get word index
     assign i_out = mem[address >> 2];
+
 endmodule
