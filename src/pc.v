982a908de10506af6a389d6be33afeeeb6d04467
Matheus Eduardo da Silva Bastos <matheus.bastos@ufrpe.br>
2026-06-15 15:23:29 -0300

    feat: implemented pc

--- src/pc.v
+++ src/pc.v
@@ -1,2 +1,15 @@
-module pc;
+module pc (
+    input wire clk,
+    input wire reset,
+    input wire [31:0] nextPC,
+    output reg [31:0] currentPC
+);
+
+always @(posedge clk or posedge reset) begin
+    if (reset)
+        currentPC <= 32'h00000000;
+    else
+        currentPC <= nextPC;
+end
+
 endmodule
